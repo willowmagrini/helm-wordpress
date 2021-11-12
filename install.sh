db2d017aaadee3efd92c3f1f6622245bf3f4dd3f
willow <brmagrini@gmail.com>
2021-11-12 15:04:19 -0300

    Adiciona opção para access mode do armazenamento WP

--- install.sh
+++ install.sh
@@ -9,22 +9,24 @@ DB_VOLUME_SIZE="2Gi"
 WP_VOLUME_STORAGE_CLASS="cephfs"
 DB_VOLUME_STORAGE_CLASS="sc-databases"
 
+WP_VOLUME_ACCESSMODE="ReadWriteMany"
 
 install_help () {
    echo "
 
 install.sh --namespace=project-namespace --host=mysite.com [--wp-image=$WP_IMAGE] [--db-image=mariadb:10.3]
 
-    --namespace        namespace do projeto a ser instalado.
-    --host             hostname do projeto
+    --namespace              namespace do projeto a ser instalado.
+    --host                   hostname do projeto
     
-    --wp-image         imagem utilizada para o pod wordpress. (padrão: $WP_IMAGE)
-    --wp-volume-size   tamanho do volume do wordpress, montado em /var/www/html (padrão: $WP_VOLUME_SIZE).
-    --wp-volume-sc     storage class do volume do wordpress (padrão: $WP_VOLUME_STORAGE_CLASS).
+    --wp-image               imagem utilizada para o pod wordpress. (padrão: $WP_IMAGE)
+    --wp-volume-size         tamanho do volume do wordpress, montado em /var/www/html (padrão: $WP_VOLUME_SIZE).
+    --wp-volume-sc           storage class do volume do wordpress (padrão: $WP_VOLUME_STORAGE_CLASS).
+    --wp-volume-accessMode   acess mode do volume do wordpress (padrão: $WP_VOLUME_ACCESSMODE)
 
-    --db-image         imagem utilizada para o pod do banco de dados. (padrão: $DB_IMAGE)
-    --db-volume-size   tamanho do volume do banco de dados, montado em /var/lib/mysql (padrão: $DB_VOLUME_SIZE).
-    --db-volume-sc     storage class do volume do banco de dados (padrão: $DB_VOLUME_STORAGE_CLASS).
+    --db-image               imagem utilizada para o pod do banco de dados. (padrão: $DB_IMAGE)
+    --db-volume-size         tamanho do volume do banco de dados, montado em /var/lib/mysql (padrão: $DB_VOLUME_SIZE).
+    --db-volume-sc           storage class do volume do banco de dados (padrão: $DB_VOLUME_STORAGE_CLASS).
 "
 }
 
@@ -60,6 +62,10 @@ case $i in
             WP_VOLUME_STORAGE_CLASS="${i#*=}"
 	    shift
     ;;
+    --wp-volume-accessmode=*)
+            WP_VOLUME_ACCESSMODE="${i#*=}"
+	    shift
+    ;;
     --db-volume-sc=*)
             DB_VOLUME_STORAGE_CLASS="${i#*=}"
 	    shift
@@ -84,6 +90,7 @@ parâmetros para a crianção do ambiente:
 - WP Imagem:                   $WP_IMAGE
 - WP Volume Size:              $WP_VOLUME_SIZE
 - WP Volume Storage Class      $WP_VOLUME_STORAGE_CLASS
+- WP Volume Access Mode        $WP_VOLUME_ACCESSMODE
 
 - DB Imagem:                   $DB_IMAGE
 - DB Volume Size:              $DB_VOLUME_SIZE
@@ -102,4 +109,4 @@ fi
 echo "Instalando ambiente";
 
 
-helm install --namespace=$NAMESPACE --set "image=$WP_IMAGE,db.image=$DB_IMAGE,ingress.hosts[0].host=$HOST,ingress.hosts[0].paths[0]='/',ingress.secretName=$NAMESPACE-crt,persistence.size=$WP_VOLUME_SIZE,persistence.storageClass=$WP_VOLUME_STORAGE_CLASS,db.persistence.size=$DB_VOLUME_SIZE,db.persistence.storageClass=$DB_VOLUME_STORAGE_CLASS" $NAMESPACE ./
+helm install --namespace=$NAMESPACE --set "image=$WP_IMAGE,db.image=$DB_IMAGE,ingress.hosts[0].host=$HOST,ingress.hosts[0].paths[0]='/',ingress.secretName=$NAMESPACE-crt,persistence.size=$WP_VOLUME_SIZE,persistence.storageClass=$WP_VOLUME_STORAGE_CLASS,persistence.accessMode=$WP_VOLUME_ACCESSMODE,db.persistence.size=$DB_VOLUME_SIZE,db.persistence.storageClass=$DB_VOLUME_STORAGE_CLASS" $NAMESPACE ./
